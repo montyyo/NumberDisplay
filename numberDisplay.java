6f754b0cc78e51ccafbf06d195bb1d48d6cdea94
montyyo <marcoiban@gmail.com>
2014-11-12 11:51:23 +0100

    creacion de la clase y constructos numbredisplay

---
+++ numberDisplay.java
@@ -0,0 +1,14 @@
+
+public class numberDisplay
+{
+    // variable numerica para el display
+    private int display;
+    
+    public numberDisplay(int newDisplay)
+    {
+      display = newDisplay;
+      display = 0;
+    
+    }
+
+}
